29361578b13bb578dd45d7089f6cf92ac1f9a586
Horváth Dávid <127218549+Horvath-David@users.noreply.github.com>
2024-11-12 09:18:05 +0100

    solid-ui setup

--- solid-weather/src/App.tsx
+++ solid-weather/src/App.tsx
@@ -1,7 +1,17 @@
+import { createSignal } from "solid-js";
+import { TextField, TextFieldInput } from "~/components/ui/text-field";
+
 export function App() {
+  const [seledted, setSeledted] = createSignal(false);
+
+  const [locationInput, setLocationInput] = createSignal("");
+
   return (
-    <div class="flex h-screen w-full items-center justify-center text-3xl">
+    <div class="flex h-screen w-full flex-col items-center justify-center gap-4 text-3xl">
       Solid Weather
+      <TextField value={locationInput()} onChange={setLocationInput}>
+        <TextFieldInput type="text" placeholder="Search for a place..." />
+      </TextField>
     </div>
   );
 }
